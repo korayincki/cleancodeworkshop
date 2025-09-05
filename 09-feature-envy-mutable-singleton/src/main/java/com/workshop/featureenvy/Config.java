a7cddc3fedd95c91dab689f3d70ce4a97371dc3b
BurakKontas <aburakkontas@hotmail.com>
2025-09-05 15:20:36 +0300

    idk lgtm

--- 09-feature-envy-mutable-singleton/src/main/java/com/workshop/featureenvy/Config.java
+++ 09-feature-envy-mutable-singleton/src/main/java/com/workshop/featureenvy/Config.java
@@ -3,8 +3,8 @@ package com.workshop.featureenvy;
 // Dirty: mutable global singleton
 public class Config {
     private static Config INSTANCE = new Config();
-    public double tax = 0.18;
+    public final double tax = 0.18;
     private Config(){}
     public static Config get(){ return INSTANCE; }
-    public static void set(Config c){ INSTANCE = c; } // anyone can swap
+    private static void set(Config c){ INSTANCE = c; } // anyone can swap
 }
--- 09-feature-envy-mutable-singleton/src/test/java/com/workshop/featureenvy/StatementGeneratorTest.java
+++ 09-feature-envy-mutable-singleton/src/test/java/com/workshop/featureenvy/StatementGeneratorTest.java
@@ -5,7 +5,6 @@ import static org.junit.jupiter.api.Assertions.*;
 
 public class StatementGeneratorTest {
     @Test void computes(){
-        Config.get().tax = 0.18;
         String s = new StatementGenerator().statement(new Customer("Ada", 100, 1.0));
         assertTrue(s.contains("118.00"));
     }
