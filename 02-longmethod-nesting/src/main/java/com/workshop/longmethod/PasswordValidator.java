7434d31e14069b58e240328f726837ff816c07b5
BurakKontas <aburakkontas@hotmail.com>
2025-09-05 14:16:55 +0300

    more spray to PasswordValidator.java

--- 02-longmethod-nesting/src/main/java/com/workshop/longmethod/PasswordValidator.java
+++ 02-longmethod-nesting/src/main/java/com/workshop/longmethod/PasswordValidator.java
@@ -7,33 +7,56 @@ import java.util.Set;
 public class PasswordValidator {
     private final Set<String> blacklist = new HashSet<>(Set.of("password","12345678","qwerty","letmein","welcome"));
 
-    public String validate(String p){
-        boolean ok = true;
-        String msg = "OK";
-        if(p!=null){
-            if(p.length()>=8){
-                boolean hasUpper=false, hasDigit=false, hasSpecial=false, hasRepeat=false;
-                int repeat=1;
-                char last=0;
-                for(char c: p.toCharArray()){
-                    if(Character.isUpperCase(c)) hasUpper = true;
-                    if(Character.isDigit(c)) hasDigit = true;
-                    if("!@#$%^&*".indexOf(c)>=0) hasSpecial = true;
-                    if(c==last){ repeat++; if(repeat>=3) hasRepeat=true; } else { repeat=1; last=c; }
-                }
-                if(blacklist.contains(p)) { ok=false; msg="BLACKLISTED"; }
-                else if(!hasUpper){ ok=false; msg="NO_UPPER"; }
-                else {
-                    if(!hasDigit){ ok=false; msg="NO_DIGIT"; }
-                    else {
-                        if(!hasSpecial){ ok=false; msg="NO_SPECIAL"; }
-                        else {
-                            if(hasRepeat){ ok=false; msg="REPEATING_CHARS"; }
-                        }
-                    }
-                }
-            } else { ok=false; msg="TOO_SHORT"; }
-        } else { ok=false; msg="NULL"; }
-        return ok? "OK" : msg;
+    public String validate(String password){
+        if(password == null) return "NULL";
+        if(password.length() < 8) return "TOO_SHORT";
+
+        boolean isBlacklisted = blacklist.contains(password);
+        if(isBlacklisted) return "BLACKLISTED";
+
+        boolean hasUpperCase = hasUpper(password);
+        if(!hasUpperCase) return "NO_UPPER";
+
+        boolean hasDigit = hasDigit(password);
+        if(!hasDigit) return "NO_DIGIT";
+
+        boolean hasSpecial = hasSpecial(password);
+        if(!hasSpecial) return "NO_SPECIAL";
+
+        boolean hasRepeat = hasRepeat(password);
+        if(hasRepeat) return "REPEATING_CHARS";
+
+        return "OK";
+    }
+
+    private static boolean hasUpper(String password) {
+        for(char c : password.toCharArray()) {
+            if(Character.isUpperCase(c)) return true;
+        }
+        return false;
+    }
+
+    private static boolean hasDigit(String password) {
+        for(char c : password.toCharArray()) {
+            if(Character.isDigit(c)) return true;
+        }
+        return false;
+    }
+
+    private static boolean hasSpecial(String password) {
+        for(char c : password.toCharArray()) {
+            if("!@#$%^&*".indexOf(c)>=0) return true;
+        }
+        return false;
+    }
+
+    private static boolean hasRepeat(String password) {
+        char last=0;
+        int repeat=1;
+        for(char c : password.toCharArray()) {
+            if(c==last) { repeat++; if(repeat>=3) return true; }
+            else { repeat=1; last=c; }
+        }
+        return false;
     }
 }
